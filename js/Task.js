e97843828d0e85c1b239a451f79212b02939e10c
chillabass <vovapete4kin@gmail.com>
2021-12-19 00:41:46 +0300

    add, delete,  marking tasks done! and save&restore task almost done.

--- js/Task.js
+++ js/Task.js
@@ -1,9 +1,10 @@
 'use strict'
 
 export default class Task {
-    constructor(text) {
+    constructor(text, id) {
         if (text === '') throw new Error('Текст задания не может быть пустым!');
         this._text = text;
+        this._id = id;
         this._isDone = false;
     }
     get isDone() {
@@ -12,11 +13,24 @@ export default class Task {
     set isDone(value) {
         this._isDone = value;
     }
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
+    }
     get text() {
         return this._text;
     }
     set text(text) {
         if (text === '') throw new Error('Текст задания не может быть пустым!');
         this._text = text;
+    }
+    toJSON() {
+        return {
+            id: this._id,
+            text: this._text,
+            isDone: this._isDone,
+        }
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,65 +1,118 @@
 'use strict'
-import Task from './Task.js'
+import Task from './Task.js';
 
 let tasksContainer = document.getElementById('tasks_container');
 let tasksList = document.querySelector('.list__tasks');
 
 // Массив объектов задач
-let tasks = [];
+let tasks = new Map();
+
+function showError(text, x, y, duration) {
+    let d = document.querySelector('.error__window');
+    let className = 'error__window-anim';
+    d.style.cssText = `top: ${y}px; left: ${x}px`;
+    d.firstChild.textContent = text;
+    d.classList.add(className);
+    setTimeout(() => {
+        d.classList.remove(className);
+    }, duration);
+}
 
 // Добавление задачи
 tasksContainer.addEventListener('keydown', function(event) {
     if ((event.code === 'Enter' || event.code === 'NumpadEnter') && event.target.tagName === 'INPUT') {
-        // Создаем пустой div
-        let div = document.createElement('div');
-        div.classList.add('list__tasks-wrapper');
-        // Создаем checkbox
-        let checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.classList.add('checkbox');
-        checkbox.addEventListener('change', () => {
-            div.classList.toggle('completed');
-        });
-        div.append(checkbox);
-        // Создаем label для текста задания и получаем текст из input
         let input = document.querySelector('#inputTask');
-        let label = document.createElement('label');
-        label.textContent = input.value;
+        // Если поле в иnput пустое - выводим окошко с ошибкой
+        if (input.value === '') {
+            let size = input.getBoundingClientRect();
+            let x = size.left + size.width;
+            let y = size.top;
+            showError('Текст задания не может быть пустым!', x, y, 4000);
+            return;
+        }
+        let id = Date.now();
+        let labelText = input.value;
         input.value = '';
-        label.classList.add('list__tasks-label');
-        div.append(label);
-        // Создаем кнопку закрытия
-        let button = document.createElement('button');
-        button.classList.add('task__close');
-        button.hidden = true;
-        button.addEventListener('click', () => {
-            button.parentElement.remove();
-            tasks.pop(div);
-        });
-        div.append(button);
         // Добавляем div в список задач
-        tasksList.append(div);
+        tasksList.append(createTask(id, labelText));
         // Добавляем задачу в массив
-        tasks.push(div);
-        console.log(tasks)
+        let task = new Task(labelText, id);
+        // console.log(task)
+        tasks.set(id, task);
+        // console.log(task)
     }
 });
+
+function createTask(id, text, isDone = false) {
+    // Создаем пустой div
+    let div = document.createElement('div');
+    div.classList.add('list__tasks-wrapper');
+    div.dataset.id = id;
+
+    // Создаем checkbox
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = isDone;
+    if (isDone) div.classList.toggle('completed');
+    checkbox.classList.add('checkbox');
+    checkbox.addEventListener('change', () => {
+        div.classList.toggle('completed');
+        tasks.get(div.dataset.id).isDone = checkbox.checked;
+    });
+    div.append(checkbox);
+
+    // Создаем label для текста задания
+    let label = document.createElement('label');
+    label.textContent = text;
+    label.classList.add('list__tasks-label');
+    div.append(label);
+
+    // Создаем кнопку удаления
+    let button = document.createElement('button');
+    button.classList.add('task__close');
+    button.hidden = true;
+    button.addEventListener('click', () => {
+        button.parentElement.remove();
+        tasks.delete(String(id));
+    });
+    div.append(button);
+    return div;
+}
+
 
 tasksList.addEventListener('pointerover', function(event) {
     let target = event.target;
     target = target.closest('div');
-    let index = target.children.length - 1;
-    if (target.children.length > 0 && target.children[index].tagName === 'BUTTON') {
-        target.children[target.children.length - 1].hidden = false;
+    // let index = target.children.length - 1;
+    if (target.children.length > 0 && target.lastChild.tagName === 'BUTTON') {
+        target.lastChild.hidden = false;
     }
 });
 
 tasksList.addEventListener('pointerout', function(event) {
     let target = event.target;
     target = target.closest('div');
-    let index = target.children.length - 1;
-    if (target.children.length > 0 && target.children[index].tagName === 'BUTTON') {
-        target.children[target.children.length - 1].hidden = true;
+    // let index = target.children.length - 1;
+    if (target.children.length > 0 && target.lastChild.tagName === 'BUTTON') {
+        target.lastChild.hidden = true;
     }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    let restoreData = localStorage.getItem('tasks');
+    tasks = new Map(Object.entries(JSON.parse(restoreData)));
+    console.log(tasks);
+    tasks.forEach(element => {
+        console.log(element);
+        // console.log(element.text);
+        // console.log(element.isDone);
+        tasksList.append(createTask(element.id, element.text, element.isDone));
+    });
+});
+
+window.addEventListener('beforeunload', function() {
+    let saveData = JSON.stringify(Object.fromEntries(tasks));
+    // debugger
+    localStorage.setItem('tasks', saveData);
 });
 
