3eff8f2590cc8d6af5680c50661e90e5e469a294
chillabass <vovapete4kin@gmail.com>
2021-12-21 00:52:15 +0300

    filter, delete done tasks

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ import Task from './Task.js';
 
 let tasksContainer = document.getElementById('tasks_container');
 let tasksList = document.querySelector('.list__tasks');
+let filters = document.querySelector('.list__buttons-filters');
+let clearDone = document.querySelector('#clear_done');
+let counter = document.querySelector('.list__buttons-count');
+filters.filterType = 'filterAll';   // Тип фильтрации по умолчанию
 
 // Массив объектов задач
 let tasks = new Map();
@@ -37,9 +41,8 @@ tasksContainer.addEventListener('keydown', function(event) {
         tasksList.append(createTask(id, labelText));
         // Добавляем задачу в массив
         let task = new Task(labelText, id);
-        // console.log(task)
-        tasks.set(id, task);
-        // console.log(task)
+        tasks.set(String(id), task);
+        tasks.onChange();
     }
 });
 
@@ -58,12 +61,15 @@ function createTask(id, text, isDone = false) {
     checkbox.addEventListener('change', () => {
         div.classList.toggle('completed');
         tasks.get(div.dataset.id).isDone = checkbox.checked;
+        tasks.onChange();
+        filterElementList(null);
     });
     div.append(checkbox);
 
     // Создаем label для текста задания
     let label = document.createElement('label');
     label.textContent = text;
+    label.title = 'Двойной клик - редактировать';
     label.classList.add('list__tasks-label');
     div.append(label);
 
@@ -74,11 +80,60 @@ function createTask(id, text, isDone = false) {
     button.addEventListener('click', () => {
         button.parentElement.remove();
         tasks.delete(String(id));
+        tasks.onChange();
     });
     div.append(button);
     return div;
 }
 
+function filterElementList(type) {
+    // Если вызываем фильтрацию из метода checkbox'а
+    if (type == null) {
+        type = filters.filterType;  // то фильтруем по последнему использованному типу
+    }
+    let arr = Array.from(tasks);
+    tasksList.innerHTML = '';
+    switch (type) {
+        case 'filterDone':
+            arr.filter(item => item[1].isDone === true).forEach(item => tasksList.append(createTask(item[1].id, item[1].text, item[1].isDone)));
+            break;
+        case 'filterWork':
+            arr.filter(item => item[1].isDone === false).forEach(item => tasksList.append(createTask(item[1].id, item[1].text, item[1].isDone)));
+            break;
+        case 'filterAll':
+        default:
+            arr.forEach(item => tasksList.append(createTask(item[1].id, item[1].text, item[1].isDone)));
+            break;
+    }
+}
+
+filters.addEventListener('click', function(event) {
+    let className = 'filter-selected';
+    let target = event.target;
+    if (target.tagName === 'LABEL') {
+        let selectedFilter = document.querySelector(`.${className}`);
+        target.classList.toggle(className);
+        selectedFilter.classList.toggle(className);
+        let value = target.id; 
+        filters.filterType = value;
+        console.log(value)
+        filterElementList(value);
+    }
+});
+
+clearDone.addEventListener('click', function() {
+    tasks = new Map(Array.from(tasks).filter( item => !item[1].isDone));
+    filterElementList(null);
+    tasks.onChange = () => {
+        counter.textContent = `${tasks.size} ${declOfNum(tasks.size, ['задача', 'задачи', 'задач'])}`;
+        if (Array.from(tasks).filter( item => item[1].isDone).length > 0) {
+            clearDone.style.display = 'block';
+        } else {
+            clearDone.style.display = 'none';
+        }
+    }
+    tasks.onChange();
+});
 
 tasksList.addEventListener('pointerover', function(event) {
     let target = event.target;
@@ -92,27 +147,42 @@ tasksList.addEventListener('pointerover', function(event) {
 tasksList.addEventListener('pointerout', function(event) {
     let target = event.target;
     target = target.closest('div');
-    // let index = target.children.length - 1;
     if (target.children.length > 0 && target.lastChild.tagName === 'BUTTON') {
         target.lastChild.hidden = true;
     }
 });
 
+// Возвращает правильное окончание взависимости от числа
+function declOfNum(n, textForms) {  
+    n = Math.abs(n) % 100; 
+    var n1 = n % 10;
+    if (n > 10 && n < 20) { return textForms[2]; }
+    if (n1 > 1 && n1 < 5) { return textForms[1]; }
+    if (n1 == 1) { return textForms[0]; }
+    return textForms[2];
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     let restoreData = localStorage.getItem('tasks');
     tasks = new Map(Object.entries(JSON.parse(restoreData)));
+    tasks.onChange = () => {
+        counter.textContent = `${tasks.size} ${declOfNum(tasks.size, ['задача', 'задачи', 'задач'])}`;
+        if (Array.from(tasks).filter( item => item[1].isDone).length > 0) {
+            clearDone.style.display = 'block';
+        } else {
+            clearDone.style.display = 'none';
+        }
+    }
+    tasks.onChange();
     console.log(tasks);
     tasks.forEach(element => {
         console.log(element);
-        // console.log(element.text);
-        // console.log(element.isDone);
         tasksList.append(createTask(element.id, element.text, element.isDone));
     });
 });
 
 window.addEventListener('beforeunload', function() {
     let saveData = JSON.stringify(Object.fromEntries(tasks));
-    // debugger
     localStorage.setItem('tasks', saveData);
 });
 
