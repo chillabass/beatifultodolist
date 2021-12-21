f10e2b0b3f15464dbbcd2512ee87a581cad14643
chillabass <vovapete4kin@gmail.com>
2021-12-21 23:26:48 +0300

    functionality done

--- js/index.js
+++ js/index.js
@@ -6,37 +6,37 @@ let tasksList = document.querySelector('.list__tasks');
 let filters = document.querySelector('.list__buttons-filters');
 let clearDone = document.querySelector('#clear_done');
 let counter = document.querySelector('.list__buttons-count');
+let taskInput = document.querySelector('#inputTask');
 filters.filterType = 'filterAll';   // Тип фильтрации по умолчанию
 
 // Массив объектов задач
 let tasks = new Map();
 
-function showError(text, x, y, duration) {
-    let d = document.querySelector('.error__window');
+function showError(target, text, duration) {
+    let errorObj = document.querySelector('.error__window');
     let className = 'error__window-anim';
-    d.style.cssText = `top: ${y}px; left: ${x}px`;
-    d.firstChild.textContent = text;
-    d.classList.add(className);
+    let size = target.getBoundingClientRect();
+    let x = size.left + size.width;
+    let y = size.top;
+    errorObj.style.cssText = `top: ${y}px; left: ${x}px`;
+    errorObj.firstChild.textContent = text;
+    errorObj.classList.add(className);
     setTimeout(() => {
-        d.classList.remove(className);
+        errorObj.classList.remove(className);
     }, duration);
 }
 
 // Добавление задачи
-tasksContainer.addEventListener('keydown', function(event) {
+taskInput.addEventListener('keydown', function (event) {
     if ((event.code === 'Enter' || event.code === 'NumpadEnter') && event.target.tagName === 'INPUT') {
-        let input = document.querySelector('#inputTask');
         // Если поле в иnput пустое - выводим окошко с ошибкой
-        if (input.value === '') {
-            let size = input.getBoundingClientRect();
-            let x = size.left + size.width;
-            let y = size.top;
-            showError('Текст задания не может быть пустым!', x, y, 4000);
+        if (taskInput.value === '') {
+            showError(taskInput, 'Текст задания не может быть пустым!', 4000);
             return;
         }
         let id = Date.now();
-        let labelText = input.value;
-        input.value = '';
+        let labelText = taskInput.value;
+        taskInput.value = '';
         // Добавляем div в список задач
         tasksList.append(createTask(id, labelText));
         // Добавляем задачу в массив
@@ -71,6 +71,26 @@ function createTask(id, text, isDone = false) {
     label.textContent = text;
     label.title = 'Двойной клик - редактировать';
     label.classList.add('list__tasks-label');
+    label.addEventListener('dblclick', function (event) {
+        let oldChild = div.children[1];
+        let newChild = document.createElement('input');
+        newChild.value = oldChild.textContent;
+        newChild.classList.add('edit');
+        checkbox.hidden = true;
+        newChild.addEventListener('keydown', function(event) {
+            if (event.type !== 'blur' && event.code !== 'Enter' && event.code !== 'NumpadEnter') return;
+            if (newChild.value === '') {
+                showError(newChild, 'Текст задания не может быть пустым!', 4000);
+                return;
+            }
+            oldChild.textContent = newChild.value;
+            tasks.get(String(id)).text = newChild.value;
+            div.replaceChild(oldChild, newChild);
+            checkbox.hidden = false;
+        });
+        div.replaceChild(newChild, oldChild);
+        newChild.focus();
+    });
     div.append(label);
 
     // Создаем кнопку удаления
@@ -107,26 +127,26 @@ function filterElementList(type) {
     }
 }
 
-filters.addEventListener('click', function(event) {
+filters.addEventListener('click', function (event) {
     let className = 'filter-selected';
     let target = event.target;
     if (target.tagName === 'LABEL') {
         let selectedFilter = document.querySelector(`.${className}`);
         target.classList.toggle(className);
         selectedFilter.classList.toggle(className);
-        let value = target.id; 
+        let value = target.id;
         filters.filterType = value;
         console.log(value)
         filterElementList(value);
     }
 });
 
-clearDone.addEventListener('click', function() {
-    tasks = new Map(Array.from(tasks).filter( item => !item[1].isDone));
+clearDone.addEventListener('click', function () {
+    tasks = new Map(Array.from(tasks).filter(item => !item[1].isDone));
     filterElementList(null);
     tasks.onChange = () => {
         counter.textContent = `${tasks.size} ${declOfNum(tasks.size, ['задача', 'задачи', 'задач'])}`;
-        if (Array.from(tasks).filter( item => item[1].isDone).length > 0) {
+        if (Array.from(tasks).filter(item => item[1].isDone).length > 0) {
             clearDone.style.display = 'block';
         } else {
             clearDone.style.display = 'none';
@@ -135,7 +155,7 @@ clearDone.addEventListener('click', function() {
     tasks.onChange();
 });
 
-tasksList.addEventListener('pointerover', function(event) {
+tasksList.addEventListener('pointerover', function (event) {
     let target = event.target;
     target = target.closest('div');
     // let index = target.children.length - 1;
@@ -144,7 +164,7 @@ tasksList.addEventListener('pointerover', function(event) {
     }
 });
 
-tasksList.addEventListener('pointerout', function(event) {
+tasksList.addEventListener('pointerout', function (event) {
     let target = event.target;
     target = target.closest('div');
     if (target.children.length > 0 && target.lastChild.tagName === 'BUTTON') {
@@ -153,21 +173,22 @@ tasksList.addEventListener('pointerout', function(event) {
 });
 
 // Возвращает правильное окончание взависимости от числа
-function declOfNum(n, textForms) {  
-    n = Math.abs(n) % 100; 
-    var n1 = n % 10;
+function declOfNum(n, textForms) {
+    n = Math.abs(n) % 100;
+    let n1 = n % 10;
     if (n > 10 && n < 20) { return textForms[2]; }
     if (n1 > 1 && n1 < 5) { return textForms[1]; }
     if (n1 == 1) { return textForms[0]; }
     return textForms[2];
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+// После построения ДОМ-дерева читаем из локального хранилища данные о задачах
+document.addEventListener('DOMContentLoaded', function () {
     let restoreData = localStorage.getItem('tasks');
     tasks = new Map(Object.entries(JSON.parse(restoreData)));
     tasks.onChange = () => {
         counter.textContent = `${tasks.size} ${declOfNum(tasks.size, ['задача', 'задачи', 'задач'])}`;
-        if (Array.from(tasks).filter( item => item[1].isDone).length > 0) {
+        if (Array.from(tasks).filter(item => item[1].isDone).length > 0) {
             clearDone.style.display = 'block';
         } else {
             clearDone.style.display = 'none';
@@ -181,7 +202,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-window.addEventListener('beforeunload', function() {
+// Перед уходом со страницы сохраняем задачи в локальное хранилище
+window.addEventListener('beforeunload', function () {
     let saveData = JSON.stringify(Object.fromEntries(tasks));
     localStorage.setItem('tasks', saveData);
 });
